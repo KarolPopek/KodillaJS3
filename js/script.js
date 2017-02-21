ee20019f38a730f5cd0aee276514b949a843879f
KarolPopek <karol.popek@wp.pl>
2017-02-21 19:28:55 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var  triangleArea;
+	  
+var daneA = prompt('Podaj a: ');
+var daneH= prompt('Podaj h: ');
+
+triangleArea = daneA * daneH / 2;
+
+alert('Pole trójkąta o podstawie a: ' + daneA + ' i wysokości h: ' + daneH + ' wynosi: ' + triangleArea);	  
+	  
+console.log('Pole trójkąta o podstawie a: ' + daneA + ' i wysokości h: ' + daneH + ' wynosi: ' + triangleArea);
